7902c354be8a0dc9308947fc6a486e9c80f70189
wholeheartedness <mmm5885@naver.com>
2024-01-03 17:12:30 +0900

    [일반수학 1] 2292번 문제 : 벌집

---
+++ BaekJoon/Math1/b2292/Main.java
@@ -0,0 +1,43 @@
+/*
+🖥️[일반수학 1] 2292번 문제 : 벌집
+🖥️https://www.acmicpc.net/problem/2292
+위의 그림과 같이 육각형으로 이루어진 벌집이 있다. 그림에서 보는 바와 같이 중앙의 방 1부터 시작해서 이웃하는 방에 돌아가면서 1씩 증가하는 번호를 주소로 매길 수 있다.
+숫자 N이 주어졌을 때, 벌집의 중앙 1에서 N번 방까지 최소 개수의 방을 지나서 갈 때 몇 개의 방을 지나가는지(시작과 끝을 포함하여)를 계산하는 프로그램을 작성하시오.
+예를 들면, 13까지는 3개, 58까지는 5개를 지난다.
+
+입력
+첫째 줄에 N(1 ≤ N ≤ 1,000,000,000)이 주어진다.
+
+출력
+입력으로 주어진 방까지 최소 개수의 방을 지나서 갈 때 몇 개의 방을 지나는지 출력한다.
+
+예제 입력 1
+13
+예제 출력 1
+3
+ */
+package b2292;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int a = 1;
+        int b = 0;
+        int cnt = 1;
+
+        // 1이 아닐 때 실행
+        while( n != 1 ) {
+            //7, 19, 27, 41...
+            a += (b * 6);
+            if ( n <= a) break;
+            cnt++;
+            b++;
+        }
+        System.out.println(cnt);
+    }
+}
